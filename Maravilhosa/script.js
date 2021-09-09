43b49d8974a4f3e4dfd20a4d0fdbd92935818bef
emamaia <emanuelle.maia@avanade.com>
2021-09-09 19:31:40 -0300

    Adiciona maravilhosa

---
+++ Maravilhosa/script.js
@@ -0,0 +1,8 @@
+const formulario = document.querySelector('.form__exemplo')
+const divTexto = document.querySelector('.texto__exibido')
+
+formulario.addEventListener('submit', function(evento) {
+   
+
+})
+
